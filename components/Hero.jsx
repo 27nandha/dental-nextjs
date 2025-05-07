94d44c75b975c155b897b89e1f8d0f2099f45e0e
27nandha <nandhakumar272002@gmail.com>
2025-05-07 11:51:53 +0530

    first commit

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -42,7 +42,7 @@ export default function Hero() {
   return (
     <div className="relative w-full h-[80vh]  overflow-hidden md:mt-[125px] mt-[80px]">
 
-    <div className='bg-[rgba(255,255,255,0.6)] p-5 z-10 absolute top-1/3 left-[675px] md:block hidden w-[400px] transition-transform duration-700 ease-in-out'>
+    <div className='bg-[rgba(255,255,255,0.6)] p-5 z-10 absolute top-1/3 left-1/2 md:block hidden w-[400px] transition-transform duration-700 ease-in-out'>
         <h2 className='text-5xl font-medium text-end font-cinzel'>{images[currentIndex].heading}</h2>
         <p className='text-end text-2xl font-cinzel'>{images[currentIndex].para}</p>
         <div className='text-end'>
